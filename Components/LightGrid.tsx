dc902ceb85b5b02d889d644591b7a835d3ca18c8
Satvik Shukla <62174006+deadland2002@users.noreply.github.com>
2023-10-07 16:18:10 +0530

    Project base completed

---
+++ Components/LightGrid.tsx
@@ -0,0 +1,195 @@
+import React, {useEffect, useState} from 'react';
+
+function LightGrid(props) {
+    const [rows, setRows] = useState<number>(0);
+    const [cols, setCols] = useState<number>(0);
+    const [sizeOfDot, setSizeOfDot] = useState<number>(50);
+    const [mousePositions, setMousePositions] = useState<{ [key: string]: { x: number; y: number } }>({});
+
+    useEffect(() => {
+        function resize(window: Window) {
+            const {innerWidth} = window;
+            const innerHeight = document.getElementById("Projects").clientHeight
+            setCols(Math.floor(innerWidth / sizeOfDot));
+            setRows(Math.floor(innerHeight / sizeOfDot));
+        }
+
+        window.addEventListener('resize', (e) => {
+            resize(e.target as Window);
+        });
+
+        resize(window);
+
+        return () => {
+            window.removeEventListener('resize', resize);
+        };
+    }, [sizeOfDot]);
+
+    const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>, index: string , RowIndex:number,ColIndex:number) => {
+        const {clientX, clientY} = event;
+        const divRect = event.currentTarget.getBoundingClientRect();
+        const centerX = divRect.left + divRect.width / 2;
+        const centerY = divRect.top + divRect.height / 2;
+
+        const deltaX = clientX - centerX;
+        const deltaY = clientY - centerY;
+
+        const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
+        const maxDistance = Math.min(divRect.width / 2, divRect.height / 2);
+
+        const normalizedDistance = Math.min(distance, maxDistance);
+        const normalizedDeltaX = (deltaX / distance) * normalizedDistance;
+        const normalizedDeltaY = (deltaY / distance) * normalizedDistance;
+
+        const Obj = {
+            [index]: {
+                x: normalizedDeltaX,
+                y: normalizedDeltaY,
+            }
+        }
+
+
+        if(!RowIndex && !ColIndex){
+            console.log("entered")
+            console.log(index,normalizedDeltaX,normalizedDeltaY)
+            return {normalizedDeltaX , normalizedDeltaY};
+        }else{
+            console.log("called")
+            const index1 = `${RowIndex+1}-${ColIndex+1}`;
+            const index2 = `${RowIndex+1}-${ColIndex}`;
+            const index3 = `${RowIndex}-${ColIndex+1}`;
+            const index4 = `${RowIndex-1}-${ColIndex-1}`;
+            const index5 = `${RowIndex-1}-${ColIndex+1}`;
+            const index6 = `${RowIndex-1}-${ColIndex}`;
+            const index7 = `${RowIndex}-${ColIndex-1}`;
+            const index8 = `${RowIndex+1}-${ColIndex-1}`;
+            const stretchDist = 20;
+            Obj[index1]={
+                x:-stretchDist,
+                y:-stretchDist
+            }
+            Obj[index2]={
+                x:0,
+                y:-stretchDist
+            }
+            Obj[index3]={
+                x:-stretchDist,
+                y:0
+            }
+            Obj[index4]={
+                x:stretchDist,
+                y:stretchDist
+            }
+            Obj[index5]={
+                x:-stretchDist,
+                y:stretchDist
+            }
+            Obj[index6]={
+                x:0,
+                y:stretchDist
+            }
+            Obj[index7]={
+                x:stretchDist,
+                y:0
+            }
+            Obj[index8]={
+                x:stretchDist,
+                y:-stretchDist
+            }
+        }
+
+
+        setMousePositions((prevPositions) => ({
+            ...prevPositions, ...Obj
+        }));
+    };
+
+    const handleMouseLeave = (index: string, RowIndex:number,ColIndex:number) => {
+        const Obj = {
+            [index]: {
+                x: 0,
+                y: 0,
+            }
+        }
+
+        const index1 = `${RowIndex+1}-${ColIndex+1}`;
+        const index2 = `${RowIndex+1}-${ColIndex}`;
+        const index3 = `${RowIndex}-${ColIndex+1}`;
+        const index4 = `${RowIndex-1}-${ColIndex-1}`;
+        const index5 = `${RowIndex-1}-${ColIndex+1}`;
+        const index6 = `${RowIndex-1}-${ColIndex}`;
+        const index7 = `${RowIndex}-${ColIndex-1}`;
+        const index8 = `${RowIndex+1}-${ColIndex-1}`;
+
+        Obj[index1]={
+            x:0,
+            y:0
+        }
+        Obj[index2]={
+            x:0,
+            y:0
+        }
+        Obj[index3]={
+            x:0,
+            y:0
+        }
+        Obj[index4]={
+            x:0,
+            y:0
+        }
+        Obj[index5]={
+            x:0,
+            y:0
+        }
+        Obj[index6]={
+            x:0,
+            y:0
+        }
+        Obj[index7]={
+            x:0,
+            y:0
+        }
+        Obj[index8]={
+            x:0,
+            y:0
+        }
+
+        setMousePositions((prevPositions) => ({
+            ...prevPositions,
+            ...Obj,
+        }));
+    };
+
+    return (
+        <div className={'flex flex-col absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'}>
+            {Array.from({length: rows}).map((row, rowIndex) => {
+                return (
+                    <div key={rowIndex} className={'flex'}>
+                        {Array.from({length: cols}).map((col, colIndex) => {
+                            const index = `${rowIndex}-${colIndex}`;
+                            return (
+                                <div
+                                    key={colIndex}
+                                    className={'text-black flex justify-center items-center'}
+                                    style={{aspectRatio: 1, width: sizeOfDot}}
+                                    onMouseMove={(e) => handleMouseMove(e, index,rowIndex,colIndex)}
+                                    onMouseLeave={() => handleMouseLeave(index,rowIndex,colIndex)}
+                                >
+                                  <span
+                                      className={'block w-2 aspect-square bg-gray-300 rounded-full'}
+                                      style={{
+                                          transform: `translate(${mousePositions[index]?.x}px, ${mousePositions[index]?.y}px)`,
+                                          transition: "all .3s"
+                                      }}
+                                  ></span>
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+export default LightGrid;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,6 +4,7 @@ import {ScrollTrigger} from "gsap/ScrollTrigger";
 import {ScrollSmoother} from "gsap-trial/ScrollSmoother";
 import anime from 'animejs/lib/anime.es.js';
 import "../styles/Home.scss"
+import LightGrid from "../../Components/LightGrid.tsx";
 
 function Home(props) {
     gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
@@ -80,26 +81,34 @@ function Home(props) {
 
 
         const section2TimelineFade = setupSectionTimeline(`.section-${section_id}`, {
-            end: "bottom",
+            end: "+=50%",
             markers: false,
             scrub: true,
-            start :"50% top"
+            start :"80% 50%"
         });
         section2TimelineFade.fromTo("#AboutSec", {
             opacity: 1,
             zIndex: 20,
+            y:"0%"
         }, {
             opacity: 0,
             zIndex: -1,
+            y:"10%",
             ease: "linear",
         });
 
 
+
+
         section_id++;
+
+
+
+
 
         const section3Timeline = setupSectionTimeline(`.section-${section_id}`, {
             end: "+=30%",
-            markers: true,
+            markers: false,
             start: "top 80%",
             scrub:true,
         });
@@ -113,12 +122,25 @@ function Home(props) {
         })
 
 
-        // gsap.timeline({repeat:-1}).to("#SkillSec_square",{
-        //     transformOrigin:"50% 50%",
-        //     rotation : 360,
-        //     duration:10,
-        //     ease:"linear"
-        // })
+        const section3Timeline2 = setupSectionTimeline(`.section-${section_id}`, {
+            end: "+=100%",
+            markers: false,
+            start: "50% 50%",
+            scrub:true,
+        });
+        section3Timeline2.fromTo("#SkillSec_square", {
+            zIndex: 20,
+            position: "fixed", // Set the position to fixed
+            top: "100px", // Set the initial top position
+            opacity: 1,
+        }, {
+            zIndex: 10,
+            ease: "power1.inOut",
+            top: "50%", // Adjust as needed
+            opacity: 0,
+            duration: 2,
+        });
+
 
         // Cleanup function
         return () => {
@@ -128,6 +150,50 @@ function Home(props) {
             // Add cleanup for other sections if needed
         };
     }, []);
+
+
+
+
+    const HandleMouseMoveProjects = (event: React.MouseEvent<HTMLDivElement>) =>{
+        const div = event.currentTarget as HTMLDivElement;
+
+        const span = div.getElementsByClassName("expand")[0] as HTMLSpanElement;
+        const reveal = div.getElementsByClassName("cardReveal")[0] as HTMLDivElement;
+
+        reveal.style.opacity = "1";
+
+        const {clientX, clientY} = event;
+        const divRect = event.currentTarget.getBoundingClientRect();
+
+        const deltaX = Math.floor(clientX - divRect.left);
+        const deltaY = Math.floor(clientY - divRect.top);
+
+        const posX = Math.min(deltaX, divRect.width);
+        const posY = Math.min(deltaY, divRect.height);
+        const normalizedDeltaX =  posX >= 0 ? posX : 0 ;
+        const normalizedDeltaY = posY >= 0 ? posY : 0;
+
+
+        span.style.left = `${normalizedDeltaX}px`
+        span.style.top = `${normalizedDeltaY}px`
+        span.style.width = "200%"
+
+        console.log(clientY,clientX)
+        console.log(deltaY,deltaX)
+        console.log(divRect.top,divRect.left)
+        // console.log(normalizedDeltaY,normalizedDeltaX)
+        console.log("......")
+    }
+
+
+
+    const HandleMouseOutProjects = (event: React.MouseEvent<HTMLDivElement>) =>{
+        const div = event.currentTarget as HTMLDivElement;
+        const span = div.getElementsByClassName("expand")[0] as HTMLSpanElement;
+        const reveal = div.getElementsByClassName("cardReveal")[0] as HTMLDivElement;
+        reveal.style.opacity = "0";
+        span.style.width = "0"
+    }
 
     return (
         <>
@@ -166,29 +232,31 @@ function Home(props) {
 
 
 
-                        <section className={"section-2 bg-white flex items-center box-border justify-between p-6 gap-4 z-20"} id={"AboutSec"}>
-                            <div className={'w-[40%] flex justify-center'} data-speed={'0.9'}>
-                                <img src={'/png/29784.png'} className={'rounded-xl shadow-2xl'}/>
-                            </div>
-                            <div className={'min-h-[300px] flex w-[60%] flex-col gap-4 box-border p-6'} id={'Sec_About_Heading'} data-speed={'0.9'}>
-                                <div className={'flex gap-2'}>
-                                    <span className={'text-black text-5xl'}>About</span>
-                                    <span className={'text-blue-500 text-5xl'}>Me</span>
+                        <section className={"section-2 relative"} id={"AboutSec"}>
+                            <div className={'bg-white flex items-center box-border justify-between p-6 gap-4 z-20 min-h-[100vh]'} id={"AboutSec_Div"}>
+                                <div className={'w-[40%] flex justify-center'} data-speed={'0.9'}>
+                                    <img src={'/png/29784.png'} className={'rounded-xl shadow-2xl'}/>
                                 </div>
-                                <span className={'text-black'}>
-                                    Greetings! I'm [Your Name], a passionate full-stack developer based in the vibrant tech landscape of India. With over three years of hands-on experience, my journey has been a thrilling exploration of the digital realm, fueled by a love for creating seamless and innovative solutions.
+                                <div className={'min-h-[300px] flex w-[60%] flex-col gap-4 box-border p-6'} id={'Sec_About_Heading'} data-speed={'0.9'}>
+                                    <div className={'flex gap-2'}>
+                                        <span className={'text-black text-5xl'}>About</span>
+                                        <span className={'text-blue-500 text-5xl'}>Me</span>
+                                    </div>
+                                    <span className={'text-black'}>
+                                    Greetings! I'm Satvik Shukla, a passionate full-stack developer based in the vibrant tech landscape of India. With over three years of hands-on experience, my journey has been a thrilling exploration of the digital realm, fueled by a love for creating seamless and innovative solutions.
                                 </span>
-                                <span className={'text-black'}>
+                                    <span className={'text-black'}>
                                     On the frontend, I specialize in crafting captivating user experiences using technologies like React, Next.js, and React Native. From responsive web applications to cross-platform mobile solutions, I thrive on turning ideas into interactive and visually stunning realities.
                                 </span>
 
-                                <span className={'text-black'}>
+                                    <span className={'text-black'}>
                                     The backend is my playground, where I wield the power of Node.js to build robust and scalable server-side applications. I'm well-versed in connecting the dots and ensuring a smooth flow of data, whether it's in the form of APIs or real-time communication.
                                 </span>
 
-                                <span className={'text-black'}>
+                                    <span className={'text-black'}>
                                     In the database realm, I navigate through PostgreSQL, MongoDB, and SQL, orchestrating the perfect symphony of structured and unstructured data. My proficiency extends to cloud environments, with AWS and Cloudflare being integral parts of my toolkit.
                                 </span>
+                                </div>
                             </div>
                         </section>
 
@@ -203,10 +271,10 @@ function Home(props) {
 
 
 
-                        <section className={"section-3"} id={"SkillSec"} style={{backgroundImage:`url('${imageUrl}')`}}>
+                        <section className={"section-3 p-10"} id={"SkillSec"} style={{backgroundImage:`url('${imageUrl}')`}} data-speed={'1.1'}>
 
 
-                            <div className={'wrapper'} data-speed={'0.8'} id={"SkillSec_square"}>
+                            <div className={'wrapper'} data-speed={'1'} id={"SkillSec_square"}>
 
 
                                 <img src={'/gif/dot_sphere.gif'} />
@@ -247,7 +315,35 @@ function Home(props) {
 
 
 
-                        <section>
+                        <section className={"section-4 p-10 bg-white project"} id={"Projects"} data-speed={'1.1'}>
+
+                            <LightGrid />
+
+                            <div className={'flex flex-col w-full h-full gap-20'}>
+                                <div className={'flex'}>
+                                    <h2 className={'text-black text-6xl z-20'}>Projects :</h2>
+                                </div>
+
+                                <div className={'cardwrapper'}>
+
+                                    {
+                                        Array.from({length: 6}).map((single)=>(
+                                            <div className={'card'} onMouseMove={HandleMouseMoveProjects} onMouseOut={HandleMouseOutProjects} datatype={"parent"}>
+                                                <div className={'cardInner'}>
+                                                    <span className={'text-black text-3xl'}>Ignitia</span>
+                                                    <span className={'text-black'}> served as a frontend developer at SenseOriginal, a startup specializing in NFC-based applications against counterfeits</span>
+                                                </div>
+                                                <div className={'cardReveal'}>
+                                                    <span className={'text-3xl'}>Stack Used : </span>
+                                                    <span className={''}>Used NextJs as the SSG for the website along with Postman for API management and Cloudinary for Image management</span>
+                                                </div>
+                                                <span className={'expand'}></span>
+                                            </div>
+                                        ))
+                                    }
+
+                                </div>
+                            </div>
 
                         </section>
                     </div>
